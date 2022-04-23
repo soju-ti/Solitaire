367abd73c5731ea45eef8364cb53dc04cb453b32
soju <sojucr7@gmail.com>
2022-04-23 14:19:01 +0530

    animated movement from stock to list

---
+++ Cards.js
@@ -0,0 +1,270 @@
+var cards = [{
+        type: 'club',
+        image: '2 of clubs',
+        order: 2,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '3 of clubs',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '4 of clubs',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '5 of clubs',
+        order: 5,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '6 of clubs',
+        order: 6,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '7 of clubs',
+        order: 7,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '8 of clubs',
+        order: 8,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '9 of clubs',
+        order: 9,
+        revealed: false
+    }, {
+        type: 'club',
+        image: '10 of clubs',
+        order: 10,
+        revealed: false
+    }, {
+        type: 'club',
+        image: 'a of clubs',
+        order: 1,
+        revealed: false
+    }, {
+        type: 'club',
+        image: 'j of clubs',
+        order: 11,
+        revealed: false
+    }, {
+        type: 'club',
+        image: 'k of clubs',
+        order: 12,
+        revealed: false
+    }, {
+        type: 'club',
+        image: 'q of clubs',
+        order: 13,
+        revealed: false
+    },
+
+
+    {
+        type: 'heart',
+        image: '2 of hearts',
+        order: 2,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '3 of hearts',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '4 of hearts',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '5 of hearts',
+        order: 5,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '6 of hearts',
+        order: 6,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '7 of hearts',
+        order: 7,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '8 of hearts',
+        order: 8,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '9 of hearts',
+        order: 9,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: '10 of hearts',
+        order: 10,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: 'a of hearts',
+        order: 1,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: 'j of hearts',
+        order: 11,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: 'k of hearts',
+        order: 12,
+        revealed: false
+    }, {
+        type: 'heart',
+        image: 'q of hearts',
+        order: 13,
+        revealed: false
+    },
+
+    {
+        type: 'diamond',
+        image: '2 of diamonds',
+        order: 2,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '3 of diamonds',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '4 of diamonds',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '5 of diamonds',
+        order: 5,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '6 of diamonds',
+        order: 6,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '7 of diamonds',
+        order: 7,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '8 of diamonds',
+        order: 8,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '9 of diamonds',
+        order: 9,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: '10 of diamonds',
+        order: 10,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: 'a of diamonds',
+        order: 1,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: 'j of diamonds',
+        order: 11,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: 'k of diamonds',
+        order: 12,
+        revealed: false
+    }, {
+        type: 'diamond',
+        image: 'q of diamonds',
+        order: 13,
+        revealed: false
+    },
+
+
+    {
+        type: 'spade',
+        image: '2 of spades',
+        order: 2,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '3 of spades',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '4 of spades',
+        order: 3,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '5 of spades',
+        order: 5,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '6 of spades',
+        order: 6,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '7 of spades',
+        order: 7,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '8 of spades',
+        order: 8,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '9 of spades',
+        order: 9,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: '10 of spades',
+        order: 10,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: 'a of spades',
+        order: 1,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: 'j of spades',
+        order: 11,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: 'k of spades',
+        order: 12,
+        revealed: false
+    }, {
+        type: 'spade',
+        image: 'q of spades',
+        order: 13,
+        revealed: false
+    },
+];
